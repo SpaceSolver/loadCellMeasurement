f0e2ef0bf9c32b8389f953074bf02be127fe2fd7
TakahiroArase <takahiro_arase@solver.co.jp>
2021-05-16 07:29:42 +0900

    ロードセル20kg版-No.2,4の計測用スクリプト登録＆キャリブレーション結果反映

--- example_20kg_4.py
+++ example_20kg_4.py
@@ -6,7 +6,7 @@ import sys
 EMULATE_HX711=False
 
 #referenceUnit = 1
-referenceUnit = -96
+referenceUnit = -94
 
 if not EMULATE_HX711:
     import RPi.GPIO as GPIO
@@ -23,8 +23,7 @@ def cleanAndExit():
     print("Bye!")
     sys.exit()
 
-#hx = HX711(5, 6)
-hx = HX711(13,19)
+hx = HX711(5, 6)
 
 # I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
 # Still need to figure out why does it change.
@@ -40,7 +39,7 @@ hx.set_reading_format("MSB", "MSB")
 # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-#hx.set_reference_unit(14)
+#hx.set_reference_unit(113)
 hx.set_reference_unit(referenceUnit)
 
 hx.reset()
